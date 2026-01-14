29129b118a4f0e9e02ad6dad741aeb4919c7287b
Matt Fiddaman <github@m.fiddaman.uk>
2026-01-14 13:01:40 +0000

    update script

---
+++ .ncurc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  workspaces: true,
+  cooldown: 10,
+  target: 'semver',
+  reject: ['*electron*', 'openid-client', 'google-protobuf'],
+  filterResults: (name, { upgradedVersionSemver }) => {
+    if (name === '@types/node') {
+      const major = parseInt(upgradedVersionSemver?.major, 10);
+      return !Number.isInteger(major) || major <= 22;
+    }
+    return true;
+  },
+};
